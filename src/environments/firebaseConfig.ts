e12cb6cc58a3145813309b8ed6f75cb902339a8a
paul <paul@downtherabbithole.io>
2020-03-29 15:19:03 +1300

    init commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyAJc8IVsXTlUe_-VHxKo3-29z4dsgeiPOg",
+  authDomain: "wechat-sales.firebaseapp.com",
+  databaseURL: "https://wechat-sales.firebaseio.com",
+  projectId: "wechat-sales",
+  storageBucket: "wechat-sales.appspot.com",
+  messagingSenderId: "326579343256",
+  appId: "1:326579343256:web:6f3c6ebd5b21cdd31ee5ed",
+  measurementId: "G-2GDZX1F8BH"
+};
